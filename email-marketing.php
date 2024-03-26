4a6ea7e627f95e918f00cf8a7595dbaa68ee8738
ericbliss <eric.rono@strathmore.edu>
2024-03-26 13:58:25 +0300

    Hide Password

--- email-marketing.php
+++ email-marketing.php
@@ -17,7 +17,7 @@ if (isset($_POST['email'])) {
       $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
       $mail->SMTPAuth = true;                               // Enable SMTP authentication
       $mail->Username = 'eric.rono@strathmore.edu';                 // SMTP username
-      $mail->Password = 'quco vopn tlcb babt';                           // SMTP password
+      $mail->Password = '******';                           // SMTP password
       $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
       $mail->Port = 587;                                    // TCP port to connect to
 
